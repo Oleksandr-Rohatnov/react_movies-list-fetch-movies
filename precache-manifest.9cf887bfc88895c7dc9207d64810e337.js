57aaae7f9351f0b237d9b0348b4d2c4461dcee98
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-09-16 10:51:03 +0300

    Updates

---
+++ precache-manifest.9cf887bfc88895c7dc9207d64810e337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd622839a9bd38f3c45e50f7f64b27a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97ec9aaa59b4f9c72326",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "e86a865570a9726680a2",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "97ec9aaa59b4f9c72326",
+    "url": "./static/js/2.f6a0f4af.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.f6a0f4af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86a865570a9726680a2",
+    "url": "./static/js/main.be7e9479.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
